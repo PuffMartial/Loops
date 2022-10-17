ee4fb16d5df3ca29f756ba471e09c71ef16329aa
Pslams_Martial <109070870+PuffMartial@users.noreply.github.com>
2022-10-17 12:57:34 +0200

    Add files via upload
    play around if you what a job interview

---
+++ loops/index.js
@@ -0,0 +1,54 @@
+const text = "Nasele Mhlambiso";
+// let finalValue = "";
+// for (let i = 0; i < text.length; i++) {
+//     finalValue = finalValue + text.charAt(i).toUpperCase()
+// }
+// console.log(finalValue);
+
+// let output = "";
+// for(let i=0; i<9; i++){
+//     console.log("i =",i);
+//     output = output + i;
+//     console.log(output);
+// }
+// console.log(output);
+
+// for(let i=0; 1<6; i++){
+//    let output =""
+//    for(let j=0; j<i+1; j++){
+//     output = output + "*";
+//     console.log(output);
+//    }
+// }
+
+// let output = "";
+// for(let i=22; i>=0; i=i-2){
+//    output = output + i
+// }
+// console.log(output);
+
+// for(let i=4; i>=0; i--){
+//    let output =""
+//    for(let j=0; j<i+1; j++){
+//     output += "*";
+//    }
+//    console.log(output);
+// }
+
+// let i = 0;
+// let finalValue = "";
+// let i = 0;
+// while(i < text.length){
+//    finalValue += text.charAt(i).toUpperCase();
+//    i++;
+// }
+// console.log(finalValue);
+
+// let i = 0;
+// let finalValue = "";
+// do{
+//    finalValue += text.charAt(i).toUpperCase();
+//    i++;
+// }
+//  while(i < text.length);
+//  console.log(finalValue);
